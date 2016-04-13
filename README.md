efcfb40cc7388fae5d0bf077c93cad136a53749a
David Marchena <dvmarchena@gmail.com>
2016-04-13 22:25:42 +0200

    v1.0.0

---
+++ README.md
@@ -0,0 +1,33 @@
+# vanilla-ready
+
+Vanilla JS equivalent of jQuery .ready(): Execute a callback function when the DOM is fully loaded.
+
+Based on [Vanilla JavaScript Document Ready (Cross-Browser)](https://gist.github.com/raymonschouwenaar/84bc7def8fbfa06ffd84) by Raymon Schouwenaar
+
+## Usage
+
+``` html
+<head>
+...
+<script src="vanilla-ready.js"></script>
+<script>
+  document.ready(function () {
+    // Place your code here
+  });
+</script>
+...
+</head>
+```
+
+## Browser support
+
+* Chrome *(latest 2)*
+* Edge *(latest 2)*
+* Firefox *(latest 2)*
+* Internet Explorer 8+
+* Opera *(latest 2)*
+* Safari *(latest 2)*
+
+## License
+
+The code is available under the [MIT license](LICENSE).
---
+++ vanilla-ready.js
@@ -0,0 +1,18 @@
+/* @preserve vanilla-ready: execute a callback function when the DOM is fully loaded. [c]2016 @dmarchena. (Based on https://gist.github.com/raymonschouwenaar/84bc7def8fbfa06ffd84 by Raymon Schouwenaar). Licensed MIT */
+!function(document) {
+
+  HTMLDocument.prototype['ready'] = function (fn) {
+    if (document.attachEvent) {
+      // IE <= 8, using attachEvent to detect browser instead of addEventListener in order to avoid errors caused by polyfills.
+      // Execute callback only with 'complete'. 'interactive' can cause some malfunctions.
+      if (document.readyState === 'complete') fn();
+      else document.attachEvent('onreadystatechange', function () {
+        if (document.readyState === 'complete') fn();
+      });
+    } else { // modern browsers
+      if (document.readyState !== 'loading') fn();
+      else if (document.addEventListener) document.addEventListener('DOMContentLoaded', fn);
+    }
+  };
+
+}(document);
